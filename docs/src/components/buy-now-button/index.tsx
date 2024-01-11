f01a7b114d5bcab321011fad2e8d43789eef2704
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2024-01-11 16:26:11 +0530

    fix(buy-now-button): fix button position
    Fixed buy now button position when we open menu, dialog or any such component

--- docs/src/components/buy-now-button/index.tsx
+++ docs/src/components/buy-now-button/index.tsx
@@ -1,9 +1,11 @@
 const BuyNowButton = () => {
   return (
-    <a className='buy-now-button mui-fixed' role='button' href='https://themeselection.com/buy-now' target='_blank'>
-      Buy Now
-      <span className='button-inner' />
-    </a>
+    <div className='buy-now-wrapper mui-fixed'>
+      <a className='buy-now-button' role='button' href='https://themeselection.com/buy-now' target='_blank'>
+        Buy Now
+        <span className='button-inner' />
+      </a>
+    </div>
   )
 }
 
--- typescript-version/full-version/src/components/buy-now-button/index.tsx
+++ typescript-version/full-version/src/components/buy-now-button/index.tsx
@@ -1,12 +1,17 @@
+// Third-party Imports
+import classnames from 'classnames'
+
 // Style Imports
 import styles from './styles.module.css'
 
 const BuyNowButton = () => {
   return (
-    <a className={styles.button} role='button' href='https://themeselection.com/buy-now' target='_blank'>
-      Buy Now
-      <span className={styles.buttonInner} />
-    </a>
+    <div className={classnames(styles.wrapper, 'mui-fixed')}>
+      <a className={styles.button} role='button' href='https://themeselection.com/buy-now' target='_blank'>
+        Buy Now
+        <span className={styles.buttonInner} />
+      </a>
+    </div>
   )
 }
 
