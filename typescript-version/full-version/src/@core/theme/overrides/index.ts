ff05c5350b25d859e42828b33e3f412608ab683a
Omkar Oza <ozaomkar2011@gmail.com>
2024-01-25 12:40:33 +0530

    feat(paper): add paper override as per design

--- typescript-version/full-version/src/@core/theme/overrides/index.ts
+++ typescript-version/full-version/src/@core/theme/overrides/index.ts
@@ -3,13 +3,14 @@ import Accordion from './accordion'
 import Autocomplete from './autocomplete'
 import avatar from './avatar'
 import button from './button'
+import paper from './paper'
 import rating from './rating'
 import tablePagination from './table-pagination'
 import tabs from './tabs'
 import typography from './typography'
 
 const overrides = () => {
-  return Object.assign({}, Accordion(), Autocomplete, avatar, button, rating, tablePagination, tabs, typography)
+  return Object.assign({}, Accordion(), Autocomplete, avatar, button, paper, rating, tablePagination, tabs, typography)
 }
 
 export default overrides
