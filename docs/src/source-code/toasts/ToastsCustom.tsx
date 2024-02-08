829d9d98ccf3e7009ef25f9d51c52447163d85bc
Aayush Makwana <aayushmakwana16@gmail.com>
2024-02-08 17:31:41 +0530

    fix(custom-toast): fix image issue in custom toast

--- docs/src/source-code/toasts/ToastsCustom.tsx
+++ docs/src/source-code/toasts/ToastsCustom.tsx
@@ -7,24 +7,20 @@ import Typography from '@mui/material/Typography'
 // Third-party Imports
 import { toast } from 'react-toastify'
 
-import useBaseUrl from '@docusaurus/useBaseUrl'
-
 const ToastsCustom = () => {
-  const avatarUrl = useBaseUrl('/images/avatars/3.png')
-
   const handleClick = () => {
     return toast(
       t => (
         <div className='w-full flex items-center justify-between'>
           <div className='flex items-center'>
-            <Avatar alt='Victor Anderson' src={avatarUrl} className='mie-3 w-10 h-10'/>
+            <Avatar alt='Victor Anderson' src='/images/avatars/3.png' className='mie-3 w-10 h-10'/>
             <div>
-              <Typography>John Doe</Typography>
+              <Typography variant='h6'>John Doe</Typography>
               <Typography variant='caption'>Sure! 8:30pm works great!</Typography>
             </div>
           </div>
           <IconButton onClick={() => toast.dismiss(t.toastProps.toastId)}>
-            <i className='ri-close-line text-xl' />
+            <i className='tabler-x text-xl' />
           </IconButton>
         </div>
       ),
@@ -40,7 +36,7 @@ const ToastsCustom = () => {
   return (
     <div className='flex text-center flex-col items-center'
     >
-      <i className='ri-pencil-line mbe-2 text-[28px]' />
+      <i className='tabler-pencil mbe-2 text-[28px]' />
       <Typography className='mbe-4' variant='h5'>Custom</Typography>
       <Typography className='mbe-3'>Make a toast using any custom content</Typography>
       <Button className='mbe-8' variant='contained' onClick={handleClick}>
