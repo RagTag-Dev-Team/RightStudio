e72df794e3a2d3a65e77bf64ea8ccc6ed2b2d22b
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2024-03-05 17:16:58 +0530

    fix(multi): fix form bugs
    Removed margin from helper-text which comes after Radio, Checkbox or Switch
    Fixed chip size on form elements' test page

--- typescript-version/full-version/src/@core/theme/overrides/index.ts
+++ typescript-version/full-version/src/@core/theme/overrides/index.ts
@@ -3,6 +3,7 @@ import Accordion from './accordion'
 import Autocomplete from './autocomplete'
 import avatar from './avatar'
 import button from './button'
+import input from './input'
 import paper from './paper'
 import Rating from './rating'
 import tablePagination from './table-pagination'
@@ -11,7 +12,20 @@ import timeline from './timeline'
 import typography from './typography'
 
 const overrides = () => {
-  return Object.assign({}, Accordion(), Autocomplete, avatar, button, paper, Rating, tablePagination, tabs, timeline, typography)
+  return Object.assign(
+    {},
+    Accordion(),
+    Autocomplete,
+    avatar,
+    button,
+    input,
+    paper,
+    Rating,
+    tablePagination,
+    tabs,
+    timeline,
+    typography
+  )
 }
 
 export default overrides
