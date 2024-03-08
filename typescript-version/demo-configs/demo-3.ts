4490bb2575ccf20eea540b62d91a8824ac644216
heet bhalodiya <heetbhalodiya007@gmail.com>
2024-03-08 11:38:36 +0530

    refactor(demo-configs): code refactor
    Updated settingCookie name
    Added disableRipple and toastPosition in all the demo-configs

--- typescript-version/demo-configs/demo-3.ts
+++ typescript-version/demo-configs/demo-3.ts
@@ -16,6 +16,9 @@
  * Another way is to clear the cookie from the browser's Application/Storage tab and then reload the page.
  */
 
+// Third-party Imports
+import type { ToastPosition } from 'react-toastify'
+
 // Type Imports
 import type { Mode, Skin, Layout, LayoutComponentPosition, LayoutComponentWidth } from '@core/types'
 
@@ -45,11 +48,13 @@ export type Config = {
   contentWidth: LayoutComponentWidth
   compactContentWidth: number
   footer: Footer
+  disableRipple: boolean
+  toastPosition: ToastPosition
 }
 
 const themeConfig: Config = {
   templateName: 'Master',
-  settingsCookieName: 'master-next-demo-3',
+  settingsCookieName: 'master-mui-next-demo-3',
   mode: 'light', // 'system', 'light', 'dark'
   skin: 'default', // 'default', 'bordered'
   semiDark: true, // true, false
@@ -68,7 +73,9 @@ const themeConfig: Config = {
     type: 'static', // 'fixed', 'static'
     contentWidth: 'compact', // 'compact', 'wide'
     detached: true //! true, false (This will not work in the Horizontal Layout)
-  }
+  },
+  disableRipple: false, // true, false
+  toastPosition: 'top-right' // 'top-right', 'top-center', 'top-left', 'bottom-right', 'bottom-center', 'bottom-left'
 }
 
 export default themeConfig
