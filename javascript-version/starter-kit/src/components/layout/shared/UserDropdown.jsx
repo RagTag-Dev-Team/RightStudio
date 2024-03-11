55f894a8d7f01c73f8831a2ee88c0d507bf48ee6
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2024-03-11 15:46:19 +0530

    fix(user-dropdown): fix email-id in starter-kits

--- javascript-version/starter-kit/src/components/layout/shared/UserDropdown.jsx
+++ javascript-version/starter-kit/src/components/layout/shared/UserDropdown.jsx
@@ -106,7 +106,7 @@ const UserDropdown = () => {
                       <Typography className='font-medium' color='text.primary'>
                         John Doe
                       </Typography>
-                      <Typography variant='caption'>admin@materio.com</Typography>
+                      <Typography variant='caption'>admin@vuexy.com</Typography>
                     </div>
                   </div>
                   <Divider className='mlb-1' />
