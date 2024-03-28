293af55e26b1e361c92756a7b01f83be59b09612
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2024-03-28 16:28:38 +0530

    docs(config): update url in the docusaurus.config.ts file

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   favicon: 'images/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://demos.pixinvent.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
