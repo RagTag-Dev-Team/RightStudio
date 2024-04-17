af697836b0cd111119b2362317edc1be725cf187
Aayush Makwana <aayushmakwana16@gmail.com>
2024-04-17 16:11:01 +0530

    fix(toast): fix toast spacing for small screen

--- docs/src/components/styled-components/AppReactToastify.tsx
+++ docs/src/components/styled-components/AppReactToastify.tsx
@@ -1,7 +1,9 @@
 // MUI Imports
 import Box from '@mui/material/Box'
+import useMediaQuery from '@mui/material/useMediaQuery'
 import { styled } from '@mui/material/styles'
 import type { BoxProps } from '@mui/material/Box'
+import type { Theme } from '@mui/material/styles'
 
 // Third-party Imports
 import 'react-toastify/dist/ReactToastify.css'
@@ -20,55 +22,70 @@ type Props = ToastContainerProps & {
 }
 
 // Styled Components
-const ToastifyWrapper = styled(Box)<BoxProps>(({ theme }) => ({
-  '& .Toastify__toast': {
-    minBlockSize: 44,
-    borderRadius: 'var(--mui-shape-borderRadius)',
-    padding: theme.spacing(1.5, 2.5),
-    backgroundColor: 'var(--mui-palette-background-paper)',
-    boxShadow: 'var(--mui-customShadows-md)',
-    '&:not(.custom-toast)': {
-      '& .Toastify__toast-body': {
-        color: 'var(--mui-palette-text-primary)'
-      },
-      '&.Toastify__toast--success': {
-        '& .Toastify__toast-icon svg': {
-          fill: 'var(--mui-palette-success-main)'
+const ToastifyWrapper = styled(Box)<BoxProps>(({ theme }) => {
+  // Hooks
+  const isSmallScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down(480))
+
+  return {
+    ...(isSmallScreen && {
+      '& .Toastify__toast-container': {
+        marginBlockStart: theme.spacing(5),
+        marginInline: theme.spacing(5),
+        width: 'calc(100vw - 40px)'
+      }
+    }),
+    '& .Toastify__toast': {
+      minBlockSize: 44,
+      borderRadius: 'var(--mui-shape-borderRadius)',
+      padding: theme.spacing(1.5, 2.5),
+      backgroundColor: 'var(--mui-palette-background-paper)',
+      boxShadow: 'var(--mui-customShadows-md)',
+      ...(isSmallScreen && {
+        marginBlockEnd: theme.spacing(4)
+      }),
+      '&:not(.custom-toast)': {
+        '& .Toastify__toast-body': {
+          color: 'var(--mui-palette-text-primary)'
+        },
+        '&.Toastify__toast--success': {
+          '& .Toastify__toast-icon svg': {
+            fill: 'var(--mui-palette-success-main)'
+          }
+        },
+        '&.Toastify__toast--error': {
+          '& .Toastify__toast-icon svg': {
+            fill: 'var(--mui-palette-error-main)'
+          }
+        },
+        '&.Toastify__toast--warning': {
+          '& .Toastify__toast-icon svg': {
+            fill: 'var(--mui-palette-warning-main)'
+          }
+        },
+        '&.Toastify__toast--info': {
+          '& .Toastify__toast-icon svg': {
+            fill: 'var(--mui-palette-info-main)'
+          }
         }
       },
-      '&.Toastify__toast--error': {
-        '& .Toastify__toast-icon svg': {
-          fill: 'var(--mui-palette-error-main)'
-        }
-      },
-      '&.Toastify__toast--warning': {
-        '& .Toastify__toast-icon svg': {
-          fill: 'var(--mui-palette-warning-main)'
-        }
-      },
-      '&.Toastify__toast--info': {
-        '& .Toastify__toast-icon svg': {
-          fill: 'var(--mui-palette-info-main)'
-        }
+      '[data-skin="bordered"] &': {
+        boxShadow: 'none',
+        border: `1px solid var(--mui-palette-divider)`
       }
     },
-    '[data-skin="bordered"] &': {
-      boxShadow: 'none',
-      border: `1px solid var(--mui-palette-divider)`
+    '& .Toastify__toast-body': {
+      margin: 0,
+      lineHeight: 1.25,
+      fontSize: theme.typography.body1.fontSize
+    },
+    '& .Toastify__toast-icon': {
+      marginRight: theme.spacing(3)
+    },
+    '& .Toastify__close-button': {
+      color: 'var(--mui-palette-text-primary)'
     }
-  },
-  '& .Toastify__toast-body': {
-    margin: 0,
-    lineHeight: 1.25,
-    fontSize: theme.typography.body1.fontSize
-  },
-  '& .Toastify__toast-icon': {
-    marginRight: theme.spacing(3)
-  },
-  '& .Toastify__close-button': {
-    color: 'var(--mui-palette-text-primary)'
   }
-}))
+})
 
 const AppReactToastify = (props: Props) => {
   const { boxProps, direction = 'ltr', ...rest } = props
