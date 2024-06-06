2b11eb8de4afd7f926a0741439529ecbcb4534c8
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2024-06-06 20:26:59 +0530

    fix(search): fix search for excludingLang

--- typescript-version/full-version/src/components/layout/shared/search/index.tsx
+++ typescript-version/full-version/src/components/layout/shared/search/index.tsx
@@ -62,6 +62,7 @@ const transformedData = data.reduce((acc: Section[], item) => {
     id: item.id,
     name: item.name,
     url: item.url,
+    excludeLang: item.excludeLang,
     icon: item.icon,
     shortcut: item.shortcut
   }
