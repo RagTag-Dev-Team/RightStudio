14149c5770fb1bab2ee24a8ba5af7374c12b2189
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2024-06-25 13:19:11 +0530

    refactor(data): update fake-db structure
    Also added API for widget-examples

--- typescript-version/full-version/src/app/api/apps/user-list/route.ts
+++ typescript-version/full-version/src/app/api/apps/user-list/route.ts
@@ -10,7 +10,7 @@
 import { NextResponse } from 'next/server'
 
 // Data Imports
-import { db } from '@/fake-db/apps/user-list'
+import { db } from '@/fake-db/apps/userList'
 
 export async function GET() {
   return NextResponse.json(db)
--- typescript-version/full-version/src/app/api/pages/widget-examples/route.ts
+++ typescript-version/full-version/src/app/api/pages/widget-examples/route.ts
@@ -10,7 +10,7 @@
 import { NextResponse } from 'next/server'
 
 // Data Imports
-import { db } from '@/fake-db/pages/userProfile'
+import { db } from '@/fake-db/pages/widgetExamples'
 
 export async function GET() {
   return NextResponse.json(db)
