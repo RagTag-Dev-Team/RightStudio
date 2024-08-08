d22f3e935a8f36d4fa32a0b1aaf735f6d76bca2c
manny <manny@mannyjmusic.com>
2024-08-08 12:12:49 -0700

    Updates Login Page

--- src/components/layout/shared/Logo.tsx
+++ src/components/layout/shared/Logo.tsx
@@ -11,7 +11,7 @@ import styled from '@emotion/styled'
 import type { VerticalNavContextProps } from '@menu/contexts/verticalNavContext'
 
 // Component Imports
-import VuexyLogo from '@core/svg/Logo'
+// import VuexyLogo from '@core/svg/Logo'
 
 // Config Imports
 import themeConfig from '@configs/themeConfig'
@@ -71,17 +71,14 @@ const Logo = ({ color }: { color?: CSSProperties['color'] }) => {
 
   return (
     <div className='flex items-center'>
-      <VuexyLogo className='text-2xl text-primary' />
-      <LogoText
-        color={color}
-        ref={logoTextRef}
-        isHovered={isHovered}
-        isCollapsed={layout === 'collapsed'}
-        transitionDuration={transitionDuration}
-        isBreakpointReached={isBreakpointReached}
-      >
-        {themeConfig.templateName}
-      </LogoText>
+      {layout === 'collapsed' && !isHovered ? (
+        <img src="/images/pages/rightstudio-icon-color.png" alt="RightStudio" className="h-8"/>
+      ) : (
+
+
+
+        <img src="/images/pages/rightstudio-logo-white-text-side.png" alt="RightStudio" className="h-8 "/>
+      )}
     </div>
   )
 }
