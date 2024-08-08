4ddb09d5445ef6b9ec5b535a9702c25adc08e87e
manny <manny@mannyjmusic.com>
2024-08-08 16:45:16 -0700

    Functioning Surreal DB client.

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,16 +1,37 @@
 // Next Imports
 import { NextResponse } from 'next/server'
-
+import { Surreal } from 'surrealdb.js'
+import { getUserRepository } from '@/surrealdb/migrations/client/user/getUserRepository'
+import { initDb } from '@/libs/surreal'
 import type { UserTable } from './users'
 
 type ResponseUser = Omit<UserTable, 'password'>
 
+
 // Mock data for demo purpose
 import { users } from './users'
+const connectionString = process.env.NEXT_PUBLIC_SURREALDB_CONNECTION
+const username = process.env.NEXT_PUBLIC_SURREALDB_USERNAME
+const password = process.env.NEXT_PUBLIC_SURREALDB_PASSWORD
+const database = process.env.NEXT_PUBLIC_SURREALDB_DB
+const namespace = process.env.NEXT_PUBLIC_SURREALDB_NS
 
 export async function POST(req: Request) {
   // Vars
   const { email, password } = await req.json()
+
+  // Connect to SurrealDB
+  const db = await initDb()
+
+
+  const rep = getUserRepository(db);
+
+
+
+ const userList = await rep.getAllUsers()
+
+  console.log(userList);
+
   const user = users.find(u => u.email === email && u.password === password)
   let response: null | ResponseUser = null
 
